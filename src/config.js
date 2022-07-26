08bd980de71086c0d926a304cd1c6b11f424e415
klld <98587362+klldme@users.noreply.github.com>
2022-07-26 22:23:16 +0000

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,32 @@
+{
+    "Account": {
+      "Email": "email@email.com",
+      "Prefix": "!"
+    },
+    "Control": { 
+      "Give full access to": ["kll.D.BOT"],
+      "Public Bot": "false" 
+    },
+    "Party": {
+      "Cosmetics": {
+        "Banner": {
+          "Banner Color": "DefaultColor24",
+          "Banner Name": "InfluencerBanner27",
+          "Season Level": 999,
+          "battle pass tier": -999
+        },
+        "Skin": "CID_NPC_Athena_Commando_M_HenchmanSummer",
+        "Backpack": "BID_A_041_FNCS21Female",
+        "Pickaxe": "Pickaxe_ID_294_CandyCane",
+        "Emote": "EID_Chilled"
+      },
+      "Platform": "WIN",
+      "Join party on invitation": "false", 
+      "Status": "✨ !help - www.klld.tk ✨"
+      
+},
+    "Friends": {
+      "Accept all friend requests": "true" 
+    } 
+   
+  }
